d1ed5c9ca8f2d640a5cb1c43e8a6003eb44248ab
Mohammad.S.M <mohammadsahebmohammadi.2006@gmail.com>
2025-03-30 23:41:01 +0330

    Add basic project skeleton

---
+++ RPS_G/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter your move ( rock , paper , scissor ) :");
+        
+        //scan
+
+        // cumputer choise
+
+        System.out.println("computer move = " + "the move");
+
+        // you win or you lose
+
+        //play again (yes/no) :
+
+        // scan the answer
+
+
+    }
+}
