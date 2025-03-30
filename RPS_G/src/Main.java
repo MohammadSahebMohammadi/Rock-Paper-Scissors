70023e0a4095242d4c1f05e345cf0212c03442eb
Mohammad.S.M <mohammadsahebmohammadi.2006@gmail.com>
2025-03-31 00:14:38 +0330

    part 2
    cumputer choise

--- RPS_G/src/Main.java
+++ RPS_G/src/Main.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your move ( rock , paper , scissor ) : ");
+        Random rand = new Random();
 
+        System.out.print("Enter your move ( rock , paper , scissor ) : ");
         String playermove = sc.nextLine();
 
-        // cumputer choise
+        int computermove_i = rand.nextInt(3);
+        String computermove_s = null ;
 
-        System.out.println("computer move = " + "the move");
+        switch (computermove_i) {
+            case 0:
+                computermove_s = "rock";
+                break;
+            case 1:
+                computermove_s = "paper";
+                break;
+            case 2:
+                computermove_s = "scissor";
+        }
+
+        System.out.println("computer move = " + computermove_s );
 
         // you win or you lose
 
